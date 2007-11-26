57543ec8473c1315fad73fb09637b8ae071857f7
jendave <jendave@d1ed97d1-c216-0410-85de-af8bbd026197>
2007-11-26 19:12:57 +0000

    rollback prebuild. Add Sam's debian script.

---
+++ trunk/debian.sh
@@ -0,0 +1,21 @@
+version=$(date +2.0.0.svn%Y%m%d)
+pkgdir=tao-$version
+rm -Rf $pkgdir
+mkdir $pkgdir
+
+for subdir in OpenGl DevIl FFmpeg Lua Ode PhysFs Sdl; do
+   (cd src/Tao.$subdir
+    rm -Rf autotools
+    mono ../../other/Prebuild/Prebuild.exe /target autotools
+    cd autotools/Tao.$subdir
+    sed -i 's/--add-missing/& --copy/' autogen.sh */autogen.sh
+    sed -i 's/\(EXTRA_DIST=\)\(install-sh.*\)/\1"\2"/' configure.ac
+    NOCONFIGURE=1 /bin/sh autogen.sh
+    cd ../../../../
+    mv src/Tao.$subdir/autotools/Tao.$subdir $pkgdir/
+    rm -Rf src/Tao.$subdir/autotools)
+done
+
+tar czf tao_$version.orig.tar.gz $pkgdir
+rm -Rf $pkgdir
+

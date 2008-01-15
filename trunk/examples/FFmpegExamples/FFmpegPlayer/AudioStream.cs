e0abed76e8320c795181f504e4046fa911c43b00
jendave <jendave@d1ed97d1-c216-0410-85de-af8bbd026197>
2008-01-15 22:46:04 +0000

    ffmpeg was missing from the overall nant build. removed Tao.OpenAL from the ffmpeg example so ffmpeg build is inpedendant

--- trunk/examples/FFmpegExamples/FFmpegPlayer/AudioStream.cs
+++ trunk/examples/FFmpegExamples/FFmpegPlayer/AudioStream.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Tao.OpenAl;
+//using Tao.OpenAl;
 using System.Threading;
 using System.Diagnostics;
 
@@ -11,7 +11,7 @@ namespace FFmpegExamples
     {
         private const int MAX_BUFFERS = 10;
         private Decoder decoder;
-        private int source;
+        //private int source;
         private int[] buffers = new int[MAX_BUFFERS];
         private float[] zeros = { 0.0f, 0.0f, 0.0f };
         private bool playing;
@@ -23,18 +23,18 @@ namespace FFmpegExamples
             decoder = new Decoder();
             decoder.LivtUpdateEvent += new LiveUpdateCallback(decoder_LivtUpdateEvent);
 
-            Alut.alutInit();
-            Al.alGenBuffers(MAX_BUFFERS, buffers);
+            //Alut.alutInit();
+            //Al.alGenBuffers(MAX_BUFFERS, buffers);
             Check();
 
-            Al.alGenSources(1,out source);
+            //Al.alGenSources(1,out source);
             Check();
 
-            Al.alSourcefv(source, Al.AL_POSITION, zeros);
-            Al.alSourcefv(source, Al.AL_VELOCITY, zeros);
-            Al.alSourcefv(source, Al.AL_DIRECTION, zeros);
-            Al.alSourcef(source, Al.AL_ROLLOFF_FACTOR, 0.0f);
-            Al.alSourcei(source, Al.AL_SOURCE_RELATIVE, Al.AL_TRUE);
+            //Al.alSourcefv(source, Al.AL_POSITION, zeros);
+            //Al.alSourcefv(source, Al.AL_VELOCITY, zeros);
+            //Al.alSourcefv(source, Al.AL_DIRECTION, zeros);
+            //Al.alSourcef(source, Al.AL_ROLLOFF_FACTOR, 0.0f);
+            //Al.alSourcei(source, Al.AL_SOURCE_RELATIVE, Al.AL_TRUE);
         }
 
         void decoder_LivtUpdateEvent(object update)
@@ -58,21 +58,21 @@ namespace FFmpegExamples
 
         private bool Update()
         {
-            int processed;
+            int processed = 0;
             bool active = true;
 
-            Al.alGetSourcei(source, Al.AL_BUFFERS_PROCESSED, out processed);
+            //Al.alGetSourcei(source, Al.AL_BUFFERS_PROCESSED, out processed);
 
             while (processed-- > 0) {
                 int buffer = -1;
 
-                Al.alSourceUnqueueBuffers(source, 1, ref buffer);
+                //Al.alSourceUnqueueBuffers(source, 1, ref buffer);
                 Check();
 
                 active = Stream(buffer);
 
                 if (active) {
-                    Al.alSourceQueueBuffers(source, 1, ref buffer);
+                    //Al.alSourceQueueBuffers(source, 1, ref buffer);
                     Check();
                 }
             }
@@ -82,10 +82,10 @@ namespace FFmpegExamples
 
         private void Check()
         {
-            int error = Al.alGetError();
-            if (error != Al.AL_NO_ERROR) {
-                Debug.WriteLine("OpenAL error: " + Al.alGetString(error));
-            }
+            //int error = Al.alGetError();
+            //if (error != Al.AL_NO_ERROR) {
+            //    Debug.WriteLine("OpenAL error: " + Al.alGetString(error));
+            //}
         }
 
         private bool Stream(int buffer)
@@ -94,7 +94,7 @@ namespace FFmpegExamples
                 if (decoder.IsAudioStream) {
                     byte[] samples = decoder.Samples;
                     int sampleSize = decoder.SampleSize;
-                    Al.alBufferData(buffer, decoder.Format, samples, sampleSize, decoder.Frequency);
+                    //Al.alBufferData(buffer, decoder.Format, samples, sampleSize, decoder.Frequency);
                     Check();
                 }
                 return true;
@@ -104,24 +104,25 @@ namespace FFmpegExamples
 
         public bool Playing()
         {
-            int state;
-            Al.alGetSourcei(source, Al.AL_SOURCE_STATE, out state);
-            return (state == Al.AL_PLAYING);
+            //int state;
+            //Al.alGetSourcei(source, Al.AL_SOURCE_STATE, out state);
+            //return (state == Al.AL_PLAYING);
+            return true;
         }
 
         public void Stop()
         {
             playing = false;
-            Al.alSourceStop(source);
+            //Al.alSourceStop(source);
         }
 
         public bool Playback()
         {
             int queue = 0;
-            Al.alGetSourcei(source, Al.AL_BUFFERS_QUEUED, out queue);
+            //Al.alGetSourcei(source, Al.AL_BUFFERS_QUEUED, out queue);
 
             if (queue > 0) {
-                Al.alSourcePlay(source);
+                //Al.alSourcePlay(source);
             }
 
             if(Playing())
@@ -133,7 +134,7 @@ namespace FFmpegExamples
                     return false;
             }
 
-            Al.alSourceQueueBuffers(source, MAX_BUFFERS, buffers);            
+            //Al.alSourceQueueBuffers(source, MAX_BUFFERS, buffers);            
 
             return true;
         }

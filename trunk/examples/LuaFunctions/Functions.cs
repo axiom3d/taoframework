54feb91625543f6059cc2783f5e4fae429e6dddf
jendave <jendave@d1ed97d1-c216-0410-85de-af8bbd026197>
2008-02-21 17:39:45 +0000

    fixes to examples builds

--- trunk/examples/LuaFunctions/Functions.cs
+++ trunk/examples/LuaFunctions/Functions.cs
@@ -29,7 +29,7 @@ using System;
 using System.IO;
 using Tao.Lua;
 
-namespace LuaExamples
+namespace LuaFunctions
 {
     /// <summary>
     ///     Simple execution of calling a C# function from Lua.

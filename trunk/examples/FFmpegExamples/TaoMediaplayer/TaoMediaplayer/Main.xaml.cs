799b46815ea946899ea85b1c31edaf449a73f27e
seaeagle1 <seaeagle1@d1ed97d1-c216-0410-85de-af8bbd026197>
2008-03-08 00:32:19 +0000

    Adding brand new FFmpeg A/V example. It's a spin-off of my own ffmpeg testing program and because of that it works on WPF (.NET 3.0). Still a bit work-in-progress, but I think we can use a good example for this.

---
+++ trunk/examples/FFmpegExamples/TaoMediaplayer/TaoMediaplayer/Main.xaml.cs
@@ -0,0 +1,129 @@
+﻿/*
+Copyright (c) 2008 Tao Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace TaoMediaplayer
+{
+    public partial class Main : Window
+    {
+        MediaFile media;
+
+        bool paused = false;
+        bool playing = false;
+
+        private delegate void VoidDelegate();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opendialog = new OpenFileDialog();
+            opendialog.CheckFileExists = true;
+            opendialog.Multiselect = false;
+
+            if (opendialog.ShowDialog(this) == true)
+                media = new MediaFile(opendialog.FileName);
+            else
+                return;
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!playing)
+            {
+                playing = true;
+                ThreadPool.QueueUserWorkItem(videoUpdater);
+                playButton.Content = "Pause";
+            }
+            else if (playing && !paused)
+            {
+                paused = true;
+                playButton.Content = "Play";
+            }
+            else if (playing && paused)
+            {
+                paused = false;
+                playButton.Content = "Pause";
+            }
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            playing = false;
+            paused = false;
+            playButton.Content = "Play";
+            media.Rewind();
+        }        
+        
+        private AutoResetEvent waitHandle = new AutoResetEvent(true);
+
+        private void videoUpdater(object state)
+        {
+            IntPtr buffer = Marshal.AllocHGlobal(media.Width*media.Height*3);
+            try
+            {
+                while (playing)
+                {
+                    playing = media.NextVideoFrame(buffer, Tao.FFmpeg.FFmpeg.PixelFormat.PIX_FMT_RGB24);
+
+                    videoImage.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,
+                          (VoidDelegate)delegate
+                                             {
+                                                 videoImage.Source =
+                                                     BitmapSource.Create(media.Width, media.Height, 96, 96,
+                                                                         PixelFormats.Rgb24, null, buffer,
+                                                                         media.Width * media.Height * 3, (media.Width * 3 + 3) & ~3);
+                                             });
+
+                    Thread.Sleep((int)((1.0 / 25.0) * 1000));
+
+                    while (paused)
+                        Thread.Sleep((int)((1.0 / 25.0) * 1000));
+                }
+
+                media.Rewind();
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+                playButton.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                                                  (VoidDelegate) delegate
+                                                                     {
+                                                                         videoImage.Source = null;
+                                                                         playButton.Content = "Play";
+                                                                     });
+            }
+        }
+
+
+    }
+}
